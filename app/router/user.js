82202fca39f46668be121f8ac74ad95c88f352db
Koyamane <2662883144@qq.com>
2023-04-25 12:13:33 +0800

    feat: 开放用户详情接口

--- app/router/user.js
+++ app/router/user.js
@@ -4,7 +4,7 @@ module.exports = (app, jwt) => {
   app.router.post('/user/api/page', 'user.userList');
   app.router.post('/user/api/login', 'user.login');
   app.router.post('/user/api/register', 'user.register');
-  app.router.post('/user/api/info', jwt, 'user.userInfo');
+  app.router.post('/user/api/info', 'user.userInfo');
   app.router.get('/user/api/logOut', jwt, 'user.logOut');
   app.router.delete('/user/api/delete', jwt, 'user.deleteUser');
   app.router.put('/user/api/update/access', jwt, 'user.updateSomeoneAccess');
